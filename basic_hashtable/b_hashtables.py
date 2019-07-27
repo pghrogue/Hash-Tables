5ae135056639d49d6a4bc4bf66205258d97ef91d
pghrogue <jenbellpgh@gmail.com>
2019-07-27 17:16:01 -0400

    All tests passing on new linked lists

--- basic_hashtable/b_hashtables.py
+++ basic_hashtable/b_hashtables.py
@@ -7,6 +7,10 @@ class Pair:
     def __init__(self, key, value):
         self.key = key
         self.value = value
+        self.next = None
+
+    def __str__(self):
+        return "(" + self.key + ", " + self.value + ") "
 
 
 # '''
@@ -40,11 +44,18 @@ def hash(string, max):
 # '''
 def hash_table_insert(hash_table, key, value):
     index = hash(key, hash_table.capacity)
+    current_pair = hash_table.storage[index]
 
-    if hash_table.storage[index] is not None:
-        print(f"Warning: overwriting key {hash_table.storage[index].key} with value {hash_table.storage[index].value}")
+    while current_pair is not None and current_pair.key is not key:
+        current_pair = current_pair.next
 
-    hash_table.storage[index] = Pair(key, value)
+    if current_pair is None:
+        # We reached the end of the linked list - no matching keys
+        new_pair = Pair(key,value)
+        new_pair.next = hash_table.storage[index]
+        hash_table.storage[index] = new_pair
+    else:
+        current_pair.value = value  
 
 
 # '''
@@ -57,8 +68,27 @@ def hash_table_remove(hash_table, key):
 
     if hash_table.storage[index] is None:
         print(f"Warning: Index {index} does not exist! Cannot remove key {key}.")
+        return
 
-    hash_table.storage[index] = None
+    current_pair = hash_table.storage[index]
+    last_pair = None
+
+    # Loop through linked list till it hits None or finds the key
+    while current_pair is not None and current_pair.key is not key:
+        last_pair = current_pair
+        current_pair = current_pair.next
+
+    if current_pair is not None:
+        # key found - remove it
+        if last_pair is not None:
+            last_pair.next = current_pair.next
+            current_pair = None
+        else:
+            hash_table.storage[index] = None
+    else:
+        # Key not found - print warning
+        print(f"Warning: key {key} was not found.")
+
 
 
 # '''
@@ -73,7 +103,18 @@ def hash_table_retrieve(hash_table, key):
         print(f"Warning: Index {index} does not exist! Cannot retrieve key {key}.")
         return None
 
-    return hash_table.storage[index].value
+    current_pair = hash_table.storage[index]
+
+    # Loop through linked list till it finds the key or runs out of nodes
+    while current_pair is not None and current_pair.key is not key:
+        current_pair = current_pair.next
+
+    if current_pair is not None:
+        # Key found, return it
+        return current_pair.value
+    else:
+        # Key not found
+        print(f"Warning: Key {key} was not found!")
 
 
 def Testing():
@@ -87,12 +128,19 @@ def Testing():
     #     print("...gone tomorrow (success!)")
     # else:
     #     print("ERROR:  STILL HERE")
+    # ht = BasicHashTable(8)
+
+    # hash_table_insert(ht, "key-0", "new-val-0")
+    # print(ht.storage)
+    # return_value = hash_table_retrieve(ht, "key-0")
+    # print(f"Return value: {return_value}")
     ht = BasicHashTable(8)
 
-    hash_table_insert(ht, "key-0", "new-val-0")
+    hash_table_insert(ht, "key-1", "new-val-1")
+    print(f"After insert: {ht.storage}")
+    hash_table_remove(ht, "key-1")
     print(ht.storage)
-    return_value = hash_table_retrieve(ht, "key-0")
-    print(f"Return value: {return_value}")
-    
+    return_value = hash_table_retrieve(ht, "key-1")
+    print(return_value)
 
 Testing()
